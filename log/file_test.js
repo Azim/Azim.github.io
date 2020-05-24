a8b2584aa57311326b9b070834a37bd829a3b14d
Azim0ff <figaRobot@yandex.ru>
2020-05-24 16:31:17 +0300

    another fix

--- log/file_test.js
+++ log/file_test.js
@@ -8,7 +8,7 @@ fileSelector.addEventListener('change', (event) => {
         const name = file.name ? file.name : 'NOT SUPPORTED';
         const type = file.type ? file.type : 'NOT SUPPORTED';
         const size = file.size ? file.size : 'NOT SUPPORTED';
-		const content = readLog(file);
+		const content = readLog(file, li);
         output.appendChild(li);
 	}
 });
