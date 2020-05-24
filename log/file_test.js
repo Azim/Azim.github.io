7ea347537ee5f7ae1f28cf30b35d19fc438e4986
Azim0ff <figaRobot@yandex.ru>
2020-05-24 16:17:55 +0300

    test file read

---
+++ log/file_test.js
@@ -0,0 +1,22 @@
+const fileSelector = document.getElementById('file-selector');
+const output = document.getElementById('output');
+
+fileSelector.addEventListener('change', (event) => {
+	output.innerHTML = '';
+	for (const file of event.target.files) {
+		const li = document.createElement('li');
+        const name = file.name ? file.name : 'NOT SUPPORTED';
+        const type = file.type ? file.type : 'NOT SUPPORTED';
+        const size = file.size ? file.size : 'NOT SUPPORTED';
+		const content = readLog(file);
+        output.appendChild(li);
+	}
+});
+
+function readLog(file, li){
+	var reader = new FileReader();
+	reader.onload = function(){
+          li.textContent = reader.result;
+    };
+	reader.readAsText(file);
+}
